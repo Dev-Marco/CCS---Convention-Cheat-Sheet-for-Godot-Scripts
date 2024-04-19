67022e6adca9fde1b3b98367daefd5847950c59f
Dev-Marco <marco.koennecke@t-online.de>
2024-04-19 03:09:17 +0200

    Initial commit

---
+++ addons/godot_conventions_cheat_sheet/godot_conventions_cheat_sheet.gd
@@ -0,0 +1,14 @@
+@tool
+extends EditorPlugin
+
+var dock: Control
+
+func _enter_tree() -> void:
+	dock = preload('res://addons/godot_conventions_cheat_sheet/dock.tscn').instantiate()
+	dock.name = 'Conventions Cheat Sheet'
+
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_BL, dock)
+
+
+func _exit_tree() -> void:
+	remove_control_from_docks(dock)
