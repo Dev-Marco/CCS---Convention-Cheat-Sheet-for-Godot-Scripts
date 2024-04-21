d825a277672518b5518d89c7a514cf84795d3da3
Dev-Marco <marco.koennecke@t-online.de>
2024-04-21 23:44:18 +0200

    Clean up; Adjusting dock layout.

--- addons/godot_conventions_cheat_sheet/godot_conventions_cheat_sheet.gd
+++ addons/godot_conventions_cheat_sheet/godot_conventions_cheat_sheet.gd
@@ -3,6 +3,7 @@ extends EditorPlugin
 
 var dock: Control
 
+
 func _enter_tree() -> void:
 	dock = preload('res://addons/godot_conventions_cheat_sheet/dock.tscn').instantiate()
 	dock.name = 'Conventions Cheat Sheet'
@@ -12,3 +13,4 @@ func _enter_tree() -> void:
 
 func _exit_tree() -> void:
 	remove_control_from_docks(dock)
+	dock.queue_free()
